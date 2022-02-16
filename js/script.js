60c336ad17a9e4eee741c8c2e11004cc19574f00
MichelleVandes <m.vandeschrick@codeur.online>
2022-02-16 20:56:29 +0000

    détail par onglet

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+function affichDetail(typeConcours) {
+  let maclasse = document.getElementById(typeConcours);
+  maclasse.classList.remove("passInvisible");
+  maclasse.classList.add("passVisible");
+}
