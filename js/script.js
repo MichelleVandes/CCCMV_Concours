c5cefeafa4e21417dbfbad85b7e2c4c66302a7af
MichelleVandes <m.vandeschrick@codeur.online>
2022-02-17 17:09:20 +0000

    Open and close Detail OK

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
+let maclasse;;
 
 function affichDetail(typeConcours) {
-  let maclasse = document.getElementById(typeConcours);
-  maclasse.classList.remove("passInvisible");
-  maclasse.classList.add("passVisible");
+
+  if (document.getElementById("btnConcours1").innerHTML == "+ d'Info »") {
+    maclasse = document.getElementById(typeConcours);
+    maclasse.classList.remove("d-none");
+    maclasse.classList.add("d-block");
+    document.getElementById("btnConcours1").innerHTML = "- d'Info";
+
+    if (screen.width <= 767) {
+      maclasse = document.getElementById("concours2");
+      maclasse.classList.add("d-none");
+      maclasse = document.getElementById("concours3");
+      maclasse.classList.add("d-none");
+    }
+  } else {
+    closeDetail(typeConcours);
+  }
+
+}
+
+function closeDetail(typeConcours) {
+  maclasse = document.getElementById(typeConcours);
+  maclasse.classList.remove("d-block");
+  maclasse.classList.add("d-none");
+
+      maclasse = document.getElementById("concours2");
+      maclasse.classList.remove("d-none");
+
+      maclasse = document.getElementById("concours3");
+      maclasse.classList.remove("d-none");
+
+      document.getElementById("btnConcours1").innerHTML = "+ d'Info »";
+      
+    
 }
